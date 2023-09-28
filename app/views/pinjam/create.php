9709a2da3b352132e5af8094c5084fca3adaa834
s4ll <salmahr08@gmail.com>
2023-09-28 16:42:02 +0700

    StudyCases

---
+++ app/views/pinjam/create.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <h3 class="mb-3">Tambah Buku</h3>
+    <form action="<?=BASE_URL;?>/Pinjam/simpanPinjam" method="post">
+        <div class="class-body">
+           <div class="form-group mb-3">
+                <label for="nama_peminjam">Nama Peminjam</label>
+                <input type="text" class="form-control" name="nama_peminjam" id="nama_peminjam">    
+            </div>
+            <div class="form-group mb-3">
+                <label for="status">Jenis Barang</label>
+                <select class="form-control" name="jenis_barang" id="jenis_barang">
+                    <option value="Laptop">Laptop</option>
+                    <option value="HP">HP</option>
+                    <option value="Adaptor LAN">Adaptor LAN</option>
+                </select>
+            </div>
+           <div class="form-group mb-3">
+                <label for="nobarang">Nomer Barang</label>
+                <input type="number" class="form-control" name="no_barang" id="no_barang">    
+            </div>
+           <div class="form-group mb-3">
+                <label for="pinjam">Tanggal Pinjam</label>
+                <input type="datetime-local" class="form-control" name="tgl_pinjam" id="tgl_pinjam">    
+            </div>
+        </div>
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </form>
+
+</div>
